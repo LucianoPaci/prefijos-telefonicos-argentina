7ceeabeb62bedde78a21a9e1757c5c15feac8de7
Luciano Paci <lucianopaci93@gmail.com>
2020-10-05 00:37:24 -0300

    Fixed test

--- test.js
+++ test.js
@@ -1,25 +1,5 @@
 
-const fs = require("fs");
-const phoneNumbersData = require("./index");
-const startCase = require("lodash/startCase");
-
-const result = phoneNumbersData.all().map((each) => {
-  return {
-    provincia: startCase(each.province),
-    localidad: startCase(each.locality),
-    codigo: each.code,
-  };
-});
-
-fs.writeFile("cleanData.json", JSON.stringify(result), (err) => {
-  if (err) {
-    console.log("Err", err);
-    throw err;
-  }
-
-  console.log("Done Writing!");
-});
-// console.log(phoneNumbersData.findOne('code', '291'))
-// console.log(phoneNumbersData.filter('code', '291'))
-
-// console.log(phoneNumbersData.utils.groupBy('province', phoneNumbersData.filter('province', 'Buenos Aires'), (eachOne) => eachOne.code ))
+const phoneNumbersData = require("./index")
+console.log(phoneNumbersData.findOne('code', '291'))
+console.log(phoneNumbersData.filter('code', '291'))
+console.log(phoneNumbersData.utils.groupBy('province', phoneNumbersData.filter('province', 'Buenos Aires'), (eachOne) => eachOne.code ))
