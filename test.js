327e3f2f81a6edfe692296a0a839567f06abf566
Luciano Paci <lucianopaci93@gmail.com>
2020-10-05 00:38:07 -0300

    Fixed test

--- test.js
+++ test.js
@@ -2,4 +2,4 @@
 const phoneNumbersData = require("./index")
 console.log(phoneNumbersData.findOne('codigo', '291'))
 console.log(phoneNumbersData.filter('codigo', '291'))
-console.log(phoneNumbersData.utils.groupBy('provincia', phoneNumbersData.filter('provincia', 'Buenos Aires'), (eachOne) => eachOne.code ))
+console.log(phoneNumbersData.utils.groupBy('provincia', phoneNumbersData.filter('provincia', 'Buenos Aires'), (eachOne) => eachOne.codigo ))
