a362d8d6d58f340ee031eead8ee93f43098b1188
Luciano Paci <lucianopaci93@gmail.com>
2020-10-12 11:51:56 -0300

    Added getLocalitiesByPrefixes method

--- index.js
+++ index.js
@@ -32,5 +32,31 @@ module.exports = {
    */
     findOne: function (property, value) {
         return phoneNumbersData.find(data => data[property] === value)
+    },
+
+
+    /**
+     * 
+     * Retorna una array de localidades agrupados por provincia y el prefijo.
+     * @param {string[]} prefixes - Un array de prefijos telefónicos
+     */
+    getLocalitiesByPrefixes: function (prefixes = []) {
+        const filtered = []
+        prefixes.forEach(prefix => {
+            let filteredByCode = phoneNumbersData.filter('codigo', prefix)
+            if(filteredByCode.length) {
+                filtered.push(phoneNumbersData.utils.groupBy('codigo', filteredByCode, each => ({ localidad: each.localidad, provincia: each.provincia})))
+            }
+            
+        });
+        const final = []
+        
+        filtered.forEach((el) => {
+            const key = Object.keys(el)[0]
+            let orderedByProvince = phoneNumbersData.utils.groupBy('provincia', el[key], (each) => ({ localidad: each.localidad}))
+            final.push({ [key]: orderedByProvince })
+        })
+    
+        return final
     }
 }
--- test.js
+++ test.js
@@ -8,3 +8,29 @@ const phoneNumbersData = require("./index")
 // const prefijosBuenosAires = phoneNumbersData.filter('provincia', 'Buenos Aires')
 
 // console.log(phoneNumbersData.utils.groupBy('provincia', prefijosBuenosAires, (each) => each.codigo ))
+const arrayOfPrefixes = ['343', '291', '11']
+
+const filtered = []
+
+
+arrayOfPrefixes.forEach(prefix => {
+    let filteredByCode = phoneNumbersData.filter('codigo', prefix)
+    if(filteredByCode.length) {
+        filtered.push(phoneNumbersData.utils.groupBy('codigo', filteredByCode, each => ({ localidad: each.localidad, provincia: each.provincia})))
+    }
+    
+});
+const final = []
+
+filtered.forEach((el) => {
+    const key = Object.keys(el)[0]
+    let orderedByProvince = phoneNumbersData.utils.groupBy('provincia', el[key], (each) => ({ localidad: each.localidad}))
+    final.push({ [key]: orderedByProvince })
+})
+
+// const abv = phoneNumbersData.utils.groupBy('codigo', filtered, (each) => (each))
+console.log('Final', JSON.stringify(final, null, 2))
+// const ultraGroupBy = phoneNumbersData.utils.groupBy('codigo', abv, ( each )=> each)
+// console.log(phoneNumbersData.utils.groupBy('codigo', abv['Santa Fe'], each => each.localidad))
+// console.log('result', ultraGroupBy)
+
